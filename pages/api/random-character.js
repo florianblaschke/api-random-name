745f3881bd6e7fca479263ff7a18bcc94ab60c15
Florian Blaschke <florianblaschke@web.de>
2023-06-29 11:36:16 +0200

    finished

---
+++ pages/api/random-character.js
@@ -0,0 +1,12 @@
+import Chance from "chance";
+
+export default function handler(req, res) {
+  const chance = new Chance();
+  const character = {
+    firstName: chance.first(),
+    lastName: chance.last(),
+    twitter: chance.twitter(),
+    geohash: chance.geohash(),
+  };
+  res.status(200).json(character);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, isLoading, error } = useSWR("/api/random-character", fetcher);
+
+  if (isLoading || error) return null;
+  return (
+    <h1>
+      <p>{data.firstName}</p>
+      <p>{data.lastName}</p>
+      <p>{data.twitter}</p>
+      <p>{data.geohash}</p>
+    </h1>
+  );
 }
